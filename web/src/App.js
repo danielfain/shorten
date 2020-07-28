638a0968bb5efa88acaf524ee42e2ac58af55908
Daniel Fain <d6niel@protonmail.com>
2020-07-28 16:56:16 -0400

    Added bulmaswatch and fixed CSS for front page

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">shortener</h1>
+          </div>
+        </div>
+    </section>
+    <main>
+      <div style={{ display: 'flex', paddingTop: '4vh', justifyContent: 'center' }}>
+        <form style={{ width: '50vw' }}>
+          <div style={{ alignItems: 'center' }} class="field is-horizontal">
+            <label style={{ paddingRight: '1vw' }} class="label">URL</label>
+            <input style={{ maxWidth: '100%' }} class="input is-large" type="text" placeholder="Enter your URL here..." />
+            <div style={{ paddingLeft: '1vw' }} class="shorten-button">
+              <button type="submit" class="button is-primary">Go!</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
+  </div>
+);
 
 export default App;
--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulmaswatch/flatly/bulmaswatch.min.css';
+
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
