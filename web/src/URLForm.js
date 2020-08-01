e190c8d9b5e2cfeb00c4e7f1943c09169494082b
Daniel Fain <d6niel@protonmail.com>
2020-08-01 17:51:22 -0400

    Added regex for URL validation

--- web/src/URLForm.js
+++ web/src/URLForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 
 class URLForm extends Component {
     constructor(props) {
@@ -6,32 +6,48 @@ class URLForm extends Component {
         this.state = { 
           url: '' 
         };
-        
+
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {    
       this.setState({ url: event.target.value });  
     }
 
+    handleSubmit(event) {
+      let expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+      let regex = new RegExp(expression);
+
+      let url = this.state.url;
+
+      if (url.match(regex)) {
+        console.log('A URL was submitted: ' + url);
+      } else {
+        console.log('Bad URL format');
+      }
+      
+      event.preventDefault();
+    }
+
     render() {
         return (
           <div style={{ display: 'flex', paddingTop: '4vh', justifyContent: 'center' }}>
-            <form style={{ width: '50vw' }} onSubmit={e => e.preventDefault()} > 
-              <div style={{ alignItems: 'center' }} class="field is-horizontal">
-                <label style={{ paddingRight: '1vw' }} class="label">URL</label>
+            <form style={{ width: '50vw' }} onSubmit={this.handleSubmit} > 
+              <div style={{ alignItems: 'center' }} className="field is-horizontal">
+                <label style={{ paddingRight: '1vw' }} className="label">URL</label>
 
                 <input 
                   style={{ maxWidth: '100%' }} 
-                  class="input is-large" 
+                  className="input is-large" 
                   type="text" 
                   placeholder="Enter your URL here..." 
                   value={this.state.url}
                   onChange={this.handleChange} 
                 />
 
-                <div style={{ paddingLeft: '1vw' }} class="shorten-button">
-                  <button type="submit" class="button is-primary">Go!</button>
+                <div style={{ paddingLeft: '1vw' }} className="shorten-button">
+                  <button type="submit" className="button is-primary">Go!</button>
                 </div>
               </div>
             </form>
