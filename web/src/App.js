62d27b3d36a0f84ccaf01283ccea5f043e49339a
Daniel Fain <d6niel@protonmail.com>
2020-08-04 20:49:14 -0400

    Routing for URL redirection

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import Hero from './Hero';
 import URLForm from './URLForm';
+import GetURL from './GetURL';
 
 const App = () => (
   <>
-    <Hero />
-    <URLForm />
+    <Route exact path="/">
+      <Hero />
+      <URLForm />
+    </Route>
+    <Route exact path="/:id" component={GetURL} />
   </>
 );
 
